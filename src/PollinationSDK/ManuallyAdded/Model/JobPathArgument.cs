e60d03312b71b18de573d544e9f6e1d1a2fcb96e
MingboPeng <mingo1214@gmail.com>
2023-10-27 21:48:12 +0800

    fix(PathArguments): add JobRunner.UploadJobAssetsAsync()

---
+++ src/PollinationSDK/ManuallyAdded/Model/JobPathArgument.cs
@@ -0,0 +1,38 @@
+﻿
+namespace PollinationSDK
+{
+    public partial class JobPathArgument
+    {
+        public void IsAssetUploaded(bool uploaded)
+        {
+            if (this.Annotations == null)
+                this.Annotations = new System.Collections.Generic.Dictionary<string, string>();
+
+            var p = "Uploaded";
+            var v = uploaded ? "true" : "false";
+
+            if (this.Annotations.TryGetValue(p, out var path))
+            {
+                this.Annotations[p] = v;
+            }
+            else
+            {
+                this.Annotations.Add(p, v);
+            }
+        }
+
+        public bool IsAssetUploaded()
+        {
+            if (this.Annotations == null)
+                return false;
+
+            if (this.Annotations.TryGetValue("Uploaded", out var isUploaded))
+            {
+                return isUploaded == "true";
+            }
+
+            return false;
+
+        }
+    }
+}
