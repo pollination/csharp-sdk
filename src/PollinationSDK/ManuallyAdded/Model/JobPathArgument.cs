de7b04a8cdee863b0752a6bfa47e1dc98d08a9e7
MingboPeng <mingo1214@gmail.com>
2023-11-10 21:09:56 +0800

    fix(JobPathArgument): tag cloud project slug

--- src/PollinationSDK/ManuallyAdded/Model/JobPathArgument.cs
+++ src/PollinationSDK/ManuallyAdded/Model/JobPathArgument.cs
@@ -1,15 +1,14 @@
-﻿
-namespace PollinationSDK
+﻿namespace PollinationSDK
 {
     public partial class JobPathArgument
     {
-        public void IsAssetUploaded(bool uploaded)
+        private void SetAnnotation(string key, string value)
         {
             if (this.Annotations == null)
                 this.Annotations = new System.Collections.Generic.Dictionary<string, string>();
 
-            var p = "Uploaded";
-            var v = uploaded ? "true" : "false";
+            var p = key;
+            var v = value;
 
             if (this.Annotations.TryGetValue(p, out var path))
             {
@@ -19,6 +18,13 @@ namespace PollinationSDK
             {
                 this.Annotations.Add(p, v);
             }
+        }
+
+        public void IsAssetUploaded(bool uploaded)
+        {
+            var p = "Uploaded";
+            var v = uploaded ? "true" : "false";
+            SetAnnotation(p, v);
         }
 
         public bool IsAssetUploaded()
@@ -32,6 +38,34 @@ namespace PollinationSDK
             }
 
             return false;
+
+        }
+
+        public void TagCloudProjectSlug(string projSlug)
+        {
+            SetAnnotation("Project", projSlug);
+        }
+
+        public string CloudProjectSlug()
+        {
+            if (this.Annotations == null)
+                return null;
+
+            if (this.Annotations.TryGetValue("Project", out var proj))
+            {
+                return proj;
+            }
+
+            return null;
+        }
+
+        public string ToUserFriendlyString()
+        {
+            var uploaded = this.IsAssetUploaded();
+            var hint = uploaded ? "CLOUD" : "PATH";
+            var p = (this.Source.Obj as ProjectFolder)?.Path;
+            p = uploaded ? p : System.IO.Path.GetFileName(p);
+            return $"#{this.Name}:${hint}/{p}";
 
         }
     }
