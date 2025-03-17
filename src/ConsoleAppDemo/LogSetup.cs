7ca2626376101d7c2159da2027a1c54bcbec1dee
Mingbo Peng <mingo1214@gmail.com>
2025-03-17 19:27:41 -0400

    test(ConsoleApp): update Logger

---
+++ src/ConsoleAppDemo/LogSetup.cs
@@ -0,0 +1,74 @@
+﻿using Serilog.Extensions.Logging;
+using Serilog;
+using System.IO;
+using Serilog.Core;
+using Serilog.Events;
+using System.Linq;
+using System;
+
+namespace ConsoleAppDemo
+{
+    public static class LogSetup
+    {
+        private static string _logFilePath;
+        private static LoggingLevelSwitch _levelSwitch = new LoggingLevelSwitch(LogEventLevel.Information); // Initial level
+     
+        public static string LogFolder
+        {
+            get
+            {
+                var userAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                var podir = Path.Combine(userAppData, "pollination", "log");
+                if (!Directory.Exists(podir))
+                    Directory.CreateDirectory(podir);
+                return podir;
+            }
+        }
+
+        public static void OneTimeSetup(string logFileName = default)
+        {
+            var file = string.IsNullOrEmpty(logFileName) ? "log.txt" : logFileName;
+
+            _logFilePath = Path.Combine(LogFolder, file);
+
+            Serilog.Log.Logger = new Serilog.LoggerConfiguration()
+                .MinimumLevel.ControlledBy(_levelSwitch)
+                //.Enrich.FromLogContext()
+                //.Enrich.With<CallingContextEnricher>()
+                .WriteTo.File(
+                    _logFilePath,
+                    rollingInterval: RollingInterval.Hour,
+                    rollOnFileSizeLimit: true,
+                    retainedFileCountLimit: 5,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} {Properties:j}{NewLine}"
+                ).CreateLogger();
+
+            var loggerFactory = new SerilogLoggerFactory(Serilog.Log.Logger);
+            Pollination.LogHelper.OneTimeSetup(loggerFactory);
+        }
+
+        public static string GetTheLatestLog()
+        {
+            var folder = new DirectoryInfo(LogFolder);
+            if (!folder.Exists) return "";
+
+            var lastLog = folder.GetFiles().OrderBy(_ => _.LastWriteTime).LastOrDefault();
+            if (lastLog != null && lastLog.Exists) return lastLog.FullName;
+            else return "";
+        }
+
+        public static void SetLogLevel(LogEventLevel level)
+        {
+            _levelSwitch.MinimumLevel = level;
+        }
+
+        public static void Dispose()
+        {
+            if (!string.IsNullOrEmpty(_logFilePath))
+            {
+                Log.CloseAndFlush();
+            }
+
+        }
+    }
+}
---
+++ src/ConsoleAppDemo/LogUtils.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace ConsoleAppDemo
+{
+    internal static class LogUtils
+    {
+        private static string _namespaceName = System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType?.Namespace ?? "Unknown Namespace";
+        private static string _className = System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType?.Name ?? "Unknown Class";
+        private static string _logCatagory = $"{_namespaceName}.{_className}";
+
+        private static Pollination.LogHelper? _helper;
+        internal static Pollination.LogHelper Helper => _helper ??= new Pollination.LogHelper(_logCatagory);
+
+        internal static ILogger GetLogger<T>() => Helper.GetLogger<T>();
+        internal static ILogger GetLogger(Type type) => Helper.GetLogger(type);
+        internal static ILogger GetLogger(string catName) => Helper.GetLogger(catName);
+
+        internal static ILogger Logger => GetLogger(_logCatagory);
+    }
+
+}
