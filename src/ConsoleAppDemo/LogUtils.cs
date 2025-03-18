f3eb8be43f81b44606c1a2625b230699c562da96
Mingbo Peng <mingo1214@gmail.com>
2025-03-17 22:10:54 -0400

    fix(LogUtils): use GetLoggerOptional to ignore exceptions

--- src/ConsoleAppDemo/LogUtils.cs
+++ src/ConsoleAppDemo/LogUtils.cs
@@ -12,9 +12,9 @@ namespace ConsoleAppDemo
         private static Pollination.LogHelper? _helper;
         internal static Pollination.LogHelper Helper => _helper ??= new Pollination.LogHelper(_logCatagory);
 
-        internal static ILogger GetLogger<T>() => Helper.GetLogger<T>();
-        internal static ILogger GetLogger(Type type) => Helper.GetLogger(type);
-        internal static ILogger GetLogger(string catName) => Helper.GetLogger(catName);
+        internal static ILogger GetLogger<T>() => Helper.GetLoggerOptional<T>();
+        internal static ILogger GetLogger(Type type) => Helper.GetLoggerOptional(type);
+        internal static ILogger GetLogger(string catName) => Helper.GetLoggerOptional(catName);
 
         internal static ILogger Logger => GetLogger(_logCatagory);
     }
