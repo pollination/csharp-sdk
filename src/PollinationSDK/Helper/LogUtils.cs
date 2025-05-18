8380a030549363f7f4a0483ae3a872a4a0105af3
Mingbo Peng <mingo1214@gmail.com>
2025-05-17 21:13:56 -0400

    fix(logger): update with the new logger

--- src/PollinationSDK/Helper/LogUtils.cs
+++ src/PollinationSDK/Helper/LogUtils.cs
@@ -5,12 +5,10 @@ namespace PollinationSDK
 {
     internal static class LogUtils
     {
-        private static string _namespaceName = System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType?.Namespace ?? "Unknown Namespace";
-        private static string _className = System.Reflection.MethodBase.GetCurrentMethod()?.DeclaringType?.Name ?? "Unknown Class";
-        private static string _logCatagory = $"{_namespaceName}.{_className}";
+        private static string _logCatagory = $"PollinationSDK";
 
-        private static Pollination.LogHelper? _helper;
-        internal static Pollination.LogHelper Helper => _helper ??= new Pollination.LogHelper(_logCatagory);
+        private static PollinationLogger.LogHelper? _helper;
+        internal static PollinationLogger.LogHelper Helper => _helper ??= new PollinationLogger.LogHelper(_logCatagory);
 
         internal static ILogger GetLogger<T>() => Helper.GetLoggerOptional<T>();
         internal static ILogger GetLogger(Type type) => Helper.GetLoggerOptional(type);
